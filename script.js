40f313d2fecbc8cb81dedada4bb04ab0dfbb9f57
adeebsaifi50 <adeebsaifi644@gmail.com>
2025-06-27 23:08:58 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-function showSection(id) {
-  document.querySelectorAll("main section").forEach(sec => {
-    sec.classList.add("hidden");
-  });
-  document.getElementById(id).classList.remove("hidden");
-}
