a51e608ee46da72e4ebcb439978b270d38a204d9
adeebsaifi50 <adeebsaifi644@gmail.com>
2026-07-27 03:28:01 +0530

    Update script.js

--- linkverse/script.js
+++ linkverse/script.js
@@ -284,3 +284,40 @@ arr.sort((a,b)=>b.name.localeCompare(a.name));
 displayWebsites(arr);
 
 });
+let deferredPrompt;
+
+const installBtn = document.getElementById("installBtn");
+
+window.addEventListener("beforeinstallprompt", (e) => {
+
+    e.preventDefault();
+
+    deferredPrompt = e;
+
+    installBtn.style.display = "inline-block";
+
+});
+
+installBtn.addEventListener("click", async () => {
+
+    if (!deferredPrompt) return;
+
+    deferredPrompt.prompt();
+
+    const { outcome } = await deferredPrompt.userChoice;
+
+    console.log("Install:", outcome);
+
+    deferredPrompt = null;
+
+    installBtn.style.display = "none";
+
+});
+
+window.addEventListener("appinstalled", () => {
+
+    console.log("✅ LinkVerse Installed");
+
+    installBtn.style.display = "none";
+
+});
